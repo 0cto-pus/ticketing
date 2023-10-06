15860e2810c10fc748ec9c1e2bb04f2ebfcccec6
Efe <tiltergod@gmail.com>
2023-10-06 22:04:57 +0300

    fixed test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,27 +1,26 @@
-import request from 'supertest';
-import { app } from '../../app';
+import request from "supertest";
+import { app } from "../../app";
 
-it('responds with details about the current user ', async ()=>{
-   const cookie = await signin();
+it("responds with details about the current user ", async () => {
+  const cookie = await signin();
 
-   const response= await request(app)
-        .get('/api/users/currentuser')
-        .set('Cookie', cookie)
-        .send({
-            email:'test@test.com',
-            password: 'password'
-        })
-        .expect(200)
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .set("Cookie", cookie)
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(200);
 
-    expect(response.body.currentUser.email).toEqual('test@test.com')
+  expect(response.body.currentUser.email).toEqual("test@test.com");
 });
 
-it('responeds with null if not authenticated', async ()=>{
-    const response = await request(app)
-        .get('/api/users/currentuser')
-        .send()
-        .expect(200);
-    
-    expect(response.body.currentUser).toEqual(null);
-    
+it("responeds with null if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(401);
+
+  expect(response.body.currentUser).toEqual(undefined);
 });
